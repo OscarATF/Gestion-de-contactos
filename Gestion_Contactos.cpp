892f2005669861e6078fa7639c1921878b37dbd9
Oscar <oticonaf@unjbg.edu.pe>
2024-07-07 21:13:06 -0500

    Agrega funcionalidad para mostrar una lista de contactos existentes por orden de servidor de correo electronico

--- Gestion_Contactos.cpp
+++ Gestion_Contactos.cpp
@@ -69,6 +69,56 @@ void listaContacto(contactoEmail arr[], int n) {
 	system("pause");
 }
 
+void ordenEmail(contactoEmail arr[],contactoEmail arrCopia[], int& n) {
+	//genera copia de la lista
+	for (int i = 0; i < n; ++i) {
+        arrCopia[i] = arr[i];
+    }
+		int tam[n];
+		int i,j;
+		contactoEmail aux;
+		string dominio[n];
+		string auxDom;
+	//Determinar longitud de los arreglo y almacenar los dominios
+	for (i=0;i<n;i++) {
+    	tam[i] = (arrCopia[i].email).length(); //longitud caracteres
+    	for (j=0;j<tam[i];j++) {
+			if ((arrCopia[i].email)[j]=='@') {
+				while ((arrCopia[i].email)[j]!='\0') {
+					j++;
+					dominio[i]+=(arrCopia[i].email)[j];
+				}	
+			}
+		}
+	}
+	//Ordena por dominio de correo
+	for (i=0;i<n-1;i++) {
+		for (j=i+1;j<n;j++) {
+			if (dominio[i]>dominio[j]) {
+				aux=arrCopia[i];
+				arrCopia[i]=arrCopia[j];
+				arrCopia[j]=aux;
+							
+				auxDom=dominio[i];
+				dominio[i]=dominio[j];
+				dominio[j]=auxDom;
+			}
+		}
+	}
+	system("cls");
+	for (i=0;i<n;i++) {
+		cout<<"CONTACTO "<<i+1<<" :\n"<<endl;
+		cout<<"Servidor: "<<dominio[i]<<endl<<endl;
+		cout<<"Nombre: "<<arrCopia[i].nombreCompleto<<endl;
+		cout<<"Nacionalidad: "<<arrCopia[i].nacionalidad<<endl;
+		cout<<"Sexo: "<<arrCopia[i].sexo<<endl;
+		cout<<"Edad: "<<arrCopia[i].edad<<endl;
+		cout<<"Telefono: "<<arrCopia[i].telefono<<endl;
+		cout<<"Email: "<<arrCopia[i].email<<endl<<endl;
+	}
+	system("pause");
+}
+
 int main() {
 	const int MAX=1000; //Maximo numero de contactos
 	contactoEmail ListadoContactos[MAX]={
@@ -80,6 +130,7 @@ int main() {
 		{"Ricardo Diaz",'M',19,"912324573","ricardodi@yahoo.com","Boliviana"}
 	};
 	
+	contactoEmail ListadoCopia[MAX];
 	int op;
 	int n=6; //numero de contactos inicial
 	
@@ -88,8 +139,8 @@ int main() {
 		cout<<"Menu de Gestion de Contactos:"<<endl;
 		cout<<"1. Agregar un contacto"<<endl
 			<<"2. Eliminar un contacto"<<endl
-			<<"3. Mostrar listado general de contactos registrados"<<endl
-			<<"4. Mostrar listado de contactos existentes"<<endl
+			<<"3. Mostrar listado general de contactos registrados hasta el momento"<<endl
+			<<"4. Mostrar listado de contactos existentes ordenado por servidor de correo electronico"<<endl
 			<<"5. Salir del programa"<<endl;
 		cin>>op;
 		switch (op) {
@@ -103,12 +154,14 @@ int main() {
 				listaContacto(ListadoContactos,n);
 				break;
 			case 4: //ordenamiento por correo
+				ordenEmail(ListadoContactos,ListadoCopia,n);
 				break;
 			case 5: //salir del programa
 				cout<<"Saliendo del Programa"<<endl;
 				break;
 			default:
-				cout<<"Ingrese una opcion valida"<<endl;
+				cout<<"Ingrese una opcion valida\n"<<endl;
+				system("pause");
 		}
 	} while (op!=5);
 	return 0;
